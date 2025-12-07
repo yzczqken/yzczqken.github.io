e9135f31635756403c6035f399240a2aab50741f
yzczqken <yzczqken@qq.com>
2025-12-07 17:19:10 +0800

    node.js

---
+++ WahFuEstate.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.sendFile('./index.html',{root:__dirname})
+})
+app.get('/future',(req,res)=>{
+    res.sendFile('./future.html',{root:__dirname})
+})
+app.get('/history',(req,res)=>{
+    res.sendFile('./history.html',{root:__dirname})
+})
+app.get('/spot',(req,res)=>{
+    res.sendFile('./spot.html',{root:__dirname})
+})
+app.get('/story',(req,res)=>{
+    res.sendFile('./history.html',{root:__dirname})
+})
+app.get('/error',(req,res)=>{
+    res.status(404).sendFile('./error.html',{root:__dirname})
+    res.set({
+        'Content-Type': 'text/html',
+        'Refresh': '5;url=/'
+    })
+})
+app.listen(3000)
